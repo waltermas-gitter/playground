1499d02aad386b9e0d04cf3e8cf696fc946b36c8
Walter <waltermas@hotmail.com>
2022-12-29 23:23:47 +0000

    buenooo

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,30 @@
-document.write("<p>holis</p>")
+// document.write("<p>holis</p>");
+// localStorage.setItem("prueba", "holis");
+function cargarls() {
+$ls = document.getElementById("ls")
+$ls.innerHTML = ""
+$fragment = document.createDocumentFragment()
+for (var i = 0; i < localStorage.length; i++){
+    // $('body').append(localStorage.getItem(localStorage.key(i)));
+    const $li = document.createElement("li");
+        let llave = localStorage.key(i);
+        let valor = localStorage.getItem(localStorage.key(i));
+
+$li.innerHTML = `${llave}: ${valor}`
+    $fragment.appendChild($li)
+}
+$ls.appendChild($fragment)
+}
+
+
+function guardar() {
+    $data = document.getElementById("inputext")
+    console.log($data.value)
+    $key = document.getElementById("llave")
+    localStorage.setItem($key.value, $data.value)
+    $key.value = ""
+    $data.value = ""
+    cargarls()
+}
+
+cargarls()
