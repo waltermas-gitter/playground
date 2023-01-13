052ee90c2a0eb9c75c41f83dd01fa6c91358cbda
waltermas <walterjmas@gmail.com>
2023-01-13 18:37:52 -0300

    bootstrap working

--- bookmarker-extension/content3.js
+++ bookmarker-extension/content3.js
@@ -15,32 +15,65 @@ chrome.tabs.query({ active: true }, (tabs) => {
   $imagenlink.src = tab.favIconUrl;
 });
 
+//folders
+const params = {
+  apiKey: APIKEY,
+  spreadsheetId: "1q5PbYgCM4EUTxKq1RrUv-ftGNAFoDLmaiGKV6IJZacw",
+};
+const ms = Date.now();
+const url = new URL(
+  "https://api.sheetson.com/v2/sheets/bookmarker" + "?dummy=" + ms
+);
+Object.keys(params).forEach((key) =>
+  url.searchParams.append(key, encodeURIComponent(params[key]))
+);
+fetch(url, { "Cache-Control": "no-cache" })
+  .then((r) => r.json())
+  .then((json) => {
+    const carpetas = new Set();
+    const $carpetas = document.getElementById("folder-dropdown"),
+      $fragmentc = document.createDocumentFragment();
+    //                <li><a class="dropdown-item" href="#">Action</a></li>
 
-  //submit
-  $botonSubmit = document.getElementById("boton-submit-ext");
-  $botonSubmit.addEventListener("click", () => {
-    //const faviconurl = `https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${$link.value}`;
-    faviconurl = $imagenlink.src
-    const bookmark = {
-      name: $nombre.value,
-      link: $link.value,
-      desc: $desc.value,
-      folder: $folder.value,
-      favicon: faviconurl,
-    };
+    json.results.forEach((el) => {
+      carpetas.add(el.folder);
+    });
+    console.log(carpetas);
 
-    fetch("https://api.sheetson.com/v2/sheets/bookmarker", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${APIKEY}`,
-        "X-Spreadsheet-Id": "1q5PbYgCM4EUTxKq1RrUv-ftGNAFoDLmaiGKV6IJZacw",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(bookmark),
-    })
-      .then((r) => r.json())
-      .then((result) => {
-        //location.reload();
-        close();
-      });
+    carpetas.forEach((el) => {
+      const $licarpeta = document.createElement("li");
+      $licarpeta.className = "dropdown-item";
+      $licarpeta.textContent = el;
+      $fragmentc.appendChild($licarpeta);
+    });
+    $carpetas.appendChild($fragmentc);
   });
+
+//submit
+$botonSubmit = document.getElementById("boton-submit-ext");
+$botonSubmit.addEventListener("click", () => {
+  //const faviconurl = `https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${$link.value}`;
+  faviconurl = $imagenlink.src;
+  const bookmark = {
+    name: $nombre.value,
+    link: $link.value,
+    desc: $desc.value,
+    folder: $folder.value,
+    favicon: faviconurl,
+  };
+
+  fetch("https://api.sheetson.com/v2/sheets/bookmarker", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${APIKEY}`,
+      "X-Spreadsheet-Id": "1q5PbYgCM4EUTxKq1RrUv-ftGNAFoDLmaiGKV6IJZacw",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(bookmark),
+  })
+    .then((r) => r.json())
+    .then((result) => {
+      //location.reload();
+      close();
+    });
+});
