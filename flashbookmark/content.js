d7d7d0e7a7e83b0bf112beaea6b20cf04a9820bb
waltermas <walterjmas@gmail.com>
2023-01-15 16:17:34 -0300

    terminado link

--- flashbookmark/content.js
+++ flashbookmark/content.js
@@ -1,12 +1,6 @@
 const APIKEY = atob(
   "ZDJhdjVrcWRnaEo1MVQ4bFVfNkRvYUlhVHZRbzN6ZTlnbzc3RlNMLVdobVItUi1PbS1nMGVTcVo5ZFE="
 );
-const tooltipTriggerList = document.querySelectorAll(
-  '[data-bs-toggle="tooltip"]'
-);
-const tooltipList = [...tooltipTriggerList].map(
-  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
-);
 
 //folders
 const params = {
@@ -33,17 +27,69 @@ fetch(url, { "Cache-Control": "no-cache" })
     });
     console.log(carpetas);
     carpetas.forEach((el) => {
-        console.log(el);
-        const $carpeta = document.createElement("div");
-        $carpeta.innerHTML=`
-        <div class="row border border rounded m-1">
-        <p>${el}</p>
-      </div>
-        `
-        $fragment.appendChild($carpeta);
+      const $carpeta = document.createElement("div");
+      $carpeta.classList.add("row");
+      $carpeta.classList.add("border");
+      $carpeta.classList.add("rounded");
+      $carpeta.classList.add("m-1");
 
+      const $carpetaParrafo = document.createElement("p");
+      $carpetaParrafo.textContent = el;
+      $carpeta.appendChild($carpetaParrafo);
 
+      const $fragmenticons = document.createDocumentFragment();
+      json.results.forEach((elresults) => {
+        if (elresults.folder == el) {
+          const $parrafoIcon = document.createElement("div");
+          $parrafoIcon.classList.add("col-1");
+          const $ima = document.createElement("img");
+          const $linkIcon = document.createElement("a");
+          $ima.setAttribute("src", elresults.favicon);
+          $ima.setAttribute("width", 18);
+          $ima.setAttribute("height", 18);
+          $ima.classList.add("linkIcon");
+
+          $linkIcon.setAttribute("href", elresults.link);
+          $linkIcon.setAttribute("target", "_blank");
+          $linkIcon.setAttribute("data-bs-toggle", "tooltip");
+          // $linkIcon.setAttribute("data-bs-placement", "top");
+          $linkIcon.setAttribute("data-bs-container", "body");
+          $linkIcon.setAttribute("data-bs-title", elresults.name);
+          $linkIcon.appendChild($ima);
+
+          // $parrafoIcon.textContent = elresults.link;
+          $parrafoIcon.appendChild($linkIcon);
+          $fragmenticons.append($parrafoIcon);
+          // $fragmenticons.appendChild($linkIcon);
+        }
+      });
+      // $carpeta.innerHTML = `
+      //   <div class="row border border rounded m-1">
+      //   <p>${el}</p>
+      // </div>
+      //   `;
+      $carpeta.appendChild($fragmenticons);
+      $fragment.appendChild($carpeta);
     });
     $containerCarpetas.appendChild($fragment);
+    // inicializar tooltips
+    const tooltipTriggerList = document.querySelectorAll(
+      '[data-bs-toggle="tooltip"]'
+    );
+    const tooltipList = [...tooltipTriggerList].map(
+      (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+    );
+  });
 
+
+  document.addEventListener("click", (e) => {
+    if (e.target.className == "linkIcon") {
+      if (window.event.ctrlKey) {
+        console.log("eliminar");
+        e.preventDefault();
+        e.stopPropagation();
+      } else {
+        close();
+      }
+    }
   });
